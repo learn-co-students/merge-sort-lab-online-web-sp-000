306d533f56324370c1d73e886d1f9f3ce7254264
Mark A Guza <guzamaguza@gmail.com>
2019-12-15 06:05:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 
+
+function findMinAndRemoveSorted(array){
+    let min = array[0];
+    let minIndex = 0;
+  for (let i=0; i<array.length; i++){
+    if(array[i] < min){
+      min = array[i];
+      minIndex = i;
+    }
+  }
+  array.splice(minIndex, 1);
+  return min;
+}
+
+function merge(firstHalf, secondHalf){
+    let sorted = []
+    let currentMin;
+    while(firstHalf.length != 0 && secondHalf.length != 0){
+      let currentMin = findMinAndRemoveSorted(firstHalf.concat(secondHalf))
+      sorted.push(currentMin)
+    }
+    return sorted.concat(firstHalf).concat(secondHalf)
+}
+
+function mergeSort(firstHalfArray,secondHalfArray){
+    if(array.length < 2){
+      return array;
+    } else {
+      merge(mergeSort(firstHalfArray), mergeSort(secondHalfArray))
+    }
+  }
