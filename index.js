89474ea93585afe5a38bec4a557a7600d0fba627
github username <github email address>
2019-12-27 11:41:48 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 
+function findMinAndRemoveSorted(a) {
+  return a.shift();
+}
+
+function merge(aOne, aTwo) {
+  let r = [];
+  while (aOne.length > 0 && aTwo.length > 0) {
+    aOne[0] < aTwo[0] ? r.push(findMinAndRemoveSorted(aOne)) : r.push(findMinAndRemoveSorted(aTwo));
+  }
+  return r.concat(aOne).concat(aTwo);
+}
+
+function mergeSort(a) {
+  if (a.length < 2) {
+    return a;
+  } else {
+    let mid = a.length/2;
+    let firstHalf = a.slice(0, mid);
+    let secondHalf = a.slice(mid, a.length);
+    return merge(mergeSort(firstHalf), mergeSort(secondHalf));
+  }
+
+}
