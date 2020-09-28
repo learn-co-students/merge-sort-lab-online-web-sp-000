11b7fbb7335e3d50041bb2bb611da6ebdf9f5f4a
rnkwilliams <rnkwilliams07@gmail.com>
2020-09-28 12:12:58 -0500

    Done.

--- index.js
+++ index.js
@@ -17,15 +17,15 @@ function merge(arr1, arr2) {
 
 
 function mergeSort(array) {
-  let midpoint = array / 2
-  let firstHalf = array.splice(0, midpoint)
-  let secondHalf = array.splice(midpoint, array.length)
+  let midpoint = array.length / 2
+  let firstHalf = array.slice(0, midpoint)
+  let secondHalf = array.slice(midpoint, array.length)
   let sorted;
 
   if (array.length < 2) {
-    return array;
+    return array
   } else {
-    sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf));
+    sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf))
   }
   return sorted;
 }
