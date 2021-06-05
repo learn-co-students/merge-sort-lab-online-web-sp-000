b4b3357a92dd0148a312fe277ca9056780f670b7
soncrayon <arnold.curtis@gmail.com>
2021-06-05 12:11:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const findMinAndRemoveSorted = (array) => {
+    return array.shift()
+}
 
+const merge = (firstSubArray, secondSubArray) => {
+    let mergedArray = []
+    while (firstSubArray.length != 0 && secondSubArray.length != 0){
+        let firstSubArrayMin = firstSubArray[0] 
+        let secondSubArrayMin = secondSubArray[0] 
+        if (firstSubArrayMin < secondSubArrayMin){
+            mergedArray.push(findMinAndRemoveSorted(firstSubArray))
+        }else{
+            mergedArray.push(findMinAndRemoveSorted(secondSubArray))
+        }
+    }
+    return mergedArray.concat(firstSubArray).concat(secondSubArray)
+}
+
+const mergeSort = (unsorted) => {
+    let midpoint = unsorted.length/2 
+    let firstHalf = unsorted.slice(0, midpoint)
+    let secondHalf = unsorted.slice(midpoint, unsorted.length)
+    if (midpoint > 1){
+        return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+    }
+    return merge(firstHalf, secondHalf)
+}
